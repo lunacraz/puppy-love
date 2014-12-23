ffad45b9e51799a7991167f09bf0e799a812a16b
lunacraz <lunacraz@gmail.com>
2014-12-23 15:41:52 -0500

    Add in Angular js file

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,5 @@
+var puppyCards = angular.module('puppyCards',[]);
+
+puppyCards.controller('PuppyCardCtrl', function ($scope){
+	$scope.cards = []; // this has to be the all the Dogs on the site, right?
+})
